6d1a326831baa18c074eaf07ac965bea3db23e2e
Rinards Korsaks <rinardsk@gmail.com>
2022-10-31 16:23:34 +0200

    init Firebase

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// App's Firebase configuration
+export const firebaseConfig = {
+  apiKey: 'AIzaSyADEgozHuNWaAYuPesNF-phw2H-hk7_byg',
+  authDomain: 'kv-project-f7710.firebaseapp.com',
+  projectId: 'kv-project-f7710',
+  storageBucket: 'kv-project-f7710.appspot.com',
+  messagingSenderId: '976604578126',
+  appId: '1:976604578126:web:9c6b2fd06eca9e5fd51e1f',
+};
